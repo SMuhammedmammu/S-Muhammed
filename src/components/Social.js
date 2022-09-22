b6f41ed13d06581564b7cf2d615c7888f1121f97
S Muhammed <114132547+SMuhammedmammu@users.noreply.github.com>
2022-09-22 12:00:53 +0530

    Add files via upload

---
+++ src/components/Social.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Social extends Component {
+    render() {
+        return (
+            <div class="social">
+            <a href="https://codepen.io/S muhammed" target="_blank" rel="noopener noreferrer"><i class="fab fa-codepen"></i></a>
+            <a href="https://github.com/S Muhammed" target="_blank" rel="noopener noreferrer"><i class="fab fa-github"></i></a>
+            <a href="https://Instagram.com/_mu__ha__mm__ed__" target="_blank" rel="noopener noreferrer"><i class="fab fa-instagram"></i></a>
+            <a href="https://www.facebook.com/S muhammed" target="_blank" rel="noopener noreferrer"><i class="fab fa-facebook-f"></i></a>
+            <a href="https://Linkedin.com/in/S muhammed" target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin-in"></i></a>
+            <a href="https://medium.com/@smuhammed" target="_blank" rel="noopener noreferrer"><i class="fab fa-medium-m"></i></a>
+            </div>
+            )
+        }
+    }
+    
+export default Social
+    
